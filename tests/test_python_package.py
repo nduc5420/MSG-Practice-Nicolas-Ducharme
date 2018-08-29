96db7517cdc3c3212d2b0ee886235707b003c63b
nduc5420 <nicolas.ducharme1@gmail.com>
2018-08-28 23:19:26 -0600

    Update test_python_package.py

--- tests/test_python_package.py
+++ tests/test_python_package.py
@@ -9,3 +9,31 @@ def test_square():
     from python_package.trial import square
 
     assert 4 == square(2)
+
+def test_factorial():
+    "Tests the factorial function"
+
+    from python_package.trial import factorial
+
+    assert 24 == factorial(4)
+    assert 6 == factorial(3.0)
+    assert 1 == factorial(0)
+    assert 1 == factorial(-1)
+
+    with pytest.raises(ValueError):
+        factorial(3.5)
+
+def test_add_all_ints():
+    "Test the sum function"
+
+    from python_package.trial import add_all_ints
+
+    assert 55 == add_all_ints(10)
+    assert 0 == add_all_ints(0)
+    assert 1 == add_all_ints(1)
+
+    with pytest.raises(ValueError):
+        add_all_ints(3.2)
+    
+
+    
